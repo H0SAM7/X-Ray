738570cbc16d2df753ff86090ccb30dec1a1a025
Hosam Adel <hoadel2003@gmail.com>
2024-09-28 06:19:01 +0300

    "Removed OpenAI key and added newline at end of file"

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,8 +3,5 @@ import 'package:flutter/material.dart';
 const Color blueColor = Color(0xff7189FF);
 const int timerCounter = 90;
 
-const String openAiKey =
-    'sk-g13NLDbVpZtAlYkVjm8Hie-UwJOEKBA7DO-6e0IQi5T3BlbkFJgZwDs_6-yHC_7jMnA_IRY7_muJnYW6oT1IXb-7AZYA';
-
 
 const String modelPath='assets/model_dicom_cancer2.tflite';
